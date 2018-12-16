4936d6eb14c8ba0cecf3431e9988d89b75c7e0cc
LAPTOP-AD9KDN1M\Tatyana <tat.batishcheva@gmail.com>
2018-12-16 13:10:54 +0300

    Make request to server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dailyData: null,
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=20').then((response) => {
+      console.log(response);
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+      this.setState({
+        dailyData: data.results,
+      });
+    });
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="Nasa">
+        <header>NASA</header>
+        {this.state.dailyData !== null && this.state.dailyData.map(d => (
+          <div>{d.name}</div>
+        ))}
       </div>
     );
   }
